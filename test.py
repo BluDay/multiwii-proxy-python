ec2d40ad4e89e0f3417e7d5b1b75db0624421881
Engineer <bbouchemla@live.com>
2018-08-06 21:22:30 +0200

    Motor, attitude and altitude functionalities implemented

---
+++ test.py
@@ -0,0 +1,97 @@
+#! /usr/bin/python3
+
+"""
+       WiiProxy - Test script
+
+   Modifiable script for executing 
+      operations on your craft.
+
+   https://github.com/engineer-186f
+
+"""
+
+# --------------------------------------------------
+
+import json
+import serial as pyserial
+
+from os         import system
+from time       import sleep
+from WiiProxy   import MultiWii
+
+# --------------------------------------------------
+
+controller  = None
+serial      = None
+
+# --------------------------------------------------
+
+serial = pyserial.Serial()
+
+serial.port          = "/dev/ttyUSB0"
+serial.baudrate      = 115200
+serial.bytesize      = pyserial.EIGHTBITS
+serial.parity        = pyserial.PARITY_NONE
+serial.stopbits      = pyserial.STOPBITS_ONE
+serial.write_timeout = 3
+serial.xonxoff       = False
+serial.rtscts        = False
+serial.dsrdtr        = False
+
+serial.open()
+
+sleep(6)
+
+controller = MultiWii(serial)
+
+if not controller: exit()
+
+# ------- PLAYGROUND! Modify this freely -------
+
+try:
+    print("\nIdent - Information")
+    print(controller.get_ident())
+    
+    print("\nGetting channel values")
+    print(controller.get_channels(False))
+
+    print("\nGetting motor values")
+    print(controller.get_motors(False))
+
+    print("\nGetting altitude")
+    print(controller.get_altitude())
+    
+    print("\nGetting attitude")
+    print(controller.get_attitude())
+
+    print("")
+
+    """ 
+    print("Arming the craft")
+    
+    controller.arm()
+    
+    sleep(3)
+
+    print("Getting current channel values")
+    print(controller.get_channels(True))
+
+    print("Printing IMU in 2 seconds...")
+
+    sleep(2)
+
+    while True:
+        system("clear")
+
+        print(controller.get_imu(False))
+
+        sleep(0.05)
+    """
+except KeyboardInterrupt:
+    exit()
+
+# ----------------------------------------------
+
+serial.close()
+
+serial = None
