0c2bf155854e9f4c9d607dde421cfd818a7af209
BluDay <19403893+BluDay@users.noreply.github.com>
2020-06-17 22:49:53 +0200

    :)

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name        = "WiiProxy",
+    version     = "2.0",
+    description = "Python 3 module for MultiWii firmware.",
+    url         = "https://github.com/engineer-99b/WiiProxy",
+    author      = "engineer-99b",
+    license     = "MIT",
+    packages    = ["WiiProxy"]
+)
