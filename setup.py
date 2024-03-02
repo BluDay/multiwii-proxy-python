dc41e8d4112fc1b114134cacae08ccdf22ec9d64
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-03 00:11:21 +0100

    chore: update setup.py file

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 from setuptools import setup
 
@@ -6,8 +6,8 @@ setup(
     name        = "WiiProxy",
     version     = "2.0",
     description = "Python 3 module for MultiWii firmware.",
-    url         = "https://github.com/engineer-99b/WiiProxy",
-    author      = "engineer-99b",
+    url         = "https://github.com/BluDay/WiiProxy",
+    author      = "BluDay",
     license     = "MIT",
     packages    = ["WiiProxy"]
 )
