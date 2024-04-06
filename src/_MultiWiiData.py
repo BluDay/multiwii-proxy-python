54918ddf3a73e27c25026003f7de599800b25d27
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-06 23:37:20 +0200

    chore: fix weird formatting

--- src/_MultiWiiData.py
+++ src/_MultiWiiData.py
@@ -28,106 +28,106 @@ class _MultiWiiData(object):
         self._misc       = Misc()
 
     @property
-	def ident(self) -> Ident:
+    def ident(self) -> Ident:
         """Gets the..."""
         return self._ident
 
     @property
-	def status(self) -> Status:
+    def status(self) -> Status:
         """Gets the..."""
         return self._status
 
     @property
-	def raw_imu(self) -> RawImu:
+    def raw_imu(self) -> RawImu:
         """Gets the..."""
         return self._raw_imu
 
     @property
-	def servo(self) -> Servo:
+    def servo(self) -> Servo:
         """Gets the..."""
         return self._servo
 
     @property
-	def servo_conf(self) -> ServoConf:
+    def servo_conf(self) -> ServoConf:
         """Gets the..."""
         return self._servo_conf
 
     @property
-	def motor(self) -> Motor:
+    def motor(self) -> Motor:
         """Gets the..."""
         return self._motor
 
     @property
-	def motor_pins(self) -> MotorPins:
+    def motor_pins(self) -> MotorPins:
         """Gets the..."""
         return self._motor_pins
 
     @property
-	def rc(self) -> Rc:
+    def rc(self) -> Rc:
         """Gets the..."""
         return self._rc
 
     @property
-	def rc_tuning(self) -> RcTuning:
+    def rc_tuning(self) -> RcTuning:
         """Gets the..."""
         return self._rc_tuning
 
     @property
-	def attitude(self) -> Attitude:
+    def attitude(self) -> Attitude:
         """Gets the..."""
         return self._attitude
 
     @property
-	def altitude(self) -> Altitude:
+    def altitude(self) -> Altitude:
         """Gets the..."""
         return self._altitude
 
     @property
-	def raw_gps(self) -> RawGps:
+    def raw_gps(self) -> RawGps:
         """Gets the..."""
         return self._raw_gps
 
     @property
-	def comp_gps(self) -> CompGps:
+    def comp_gps(self) -> CompGps:
         """Gets the..."""
         return self._comp_gps
 
     @property
-	def waypoint(self) -> Waypoint:
+    def waypoint(self) -> Waypoint:
         """Gets the..."""
         return self._waypoint
 
     @property
-	def analog(self) -> Analog:
+    def analog(self) -> Analog:
         """Gets the..."""
         return self._analog
 
     @property
-	def pid(self) -> Pid:
+    def pid(self) -> Pid:
         """Gets the..."""
         return self._pid
 
     @property
-	def pidnames(self) -> PidNames:
+    def pidnames(self) -> PidNames:
         """Gets the..."""
         return self._pidnames
 
     @property
-	def box(self) -> Box:
+    def box(self) -> Box:
         """Gets the..."""
         return self._box
 
     @property
-	def boxnames(self) -> BoxNames:
+    def boxnames(self) -> BoxNames:
         """Gets the..."""
         return self._boxnames
 
     @property
-	def boxids(self) -> BoxIds:
+    def boxids(self) -> BoxIds:
         """Gets the..."""
         return self._boxids
 
     @property
-	def misc(self) -> Misc:
+    def misc(self) -> Misc:
         """Gets the..."""
         return self._misc
