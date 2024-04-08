269b6c3628abc9ff7c662bf95741f4894004af26
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-08 15:34:02 +0200

    chore: nothing special

--- src/multiwii/data/base/_MultiWiiNames.py
+++ src/multiwii/data/base/_MultiWiiNames.py
@@ -1,6 +1,15 @@
 class _MultiWiiNames(ValuesBase):
+    """
+    Base class for name-retrieval commands.
+    """
     def __init__(self) -> None:
+        """
+        Is this necessary?
+        """
         super().__init__()
 
-    def evaluate(self, data) -> None:
+    def update_values(self, data: tuple) -> None:
+        """
+        Overridden method.
+        """
         self.values = tuple(data[0].decode().split(';')[:-1])
