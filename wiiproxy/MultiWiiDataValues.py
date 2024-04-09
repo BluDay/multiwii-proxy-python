20a3c5f3836ca97cf5dac177005328f30920c6e8
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-10 00:32:57 +0200

    feat: add new MultiWiiCommands class

--- wiiproxy/MultiWiiDataValues.py
+++ wiiproxy/MultiWiiDataValues.py
@@ -29,49 +29,6 @@ class MultiWiiDataValues(object):
     """
     The class used for initializing all MSP data value instances and creating
     corresponding private fields for an instance of this type, or of a derived type.
-
-    Saved commands from the instance for MSP_evious codebase
-
-    {NAME} = ({CODE}, {STRUCT_FORMAT}, {HAS_VARIABLE_DATA_SIZE}, {PRIORITY_TYPE})
-
-    # Get commands.
-    MSP_IDENT      = (100, '3BI',    False, Inactive)
-    MSP_STATUS     = (101, '3HIB',   False, Inactive)
-    MSP_RAW_IMU    = (102, '9h',     False, High)
-    MSP_SERVO      = (103, '8H',     False, Normal)
-    MSP_MOTOR      = (104, '8H',     False, High)
-    MSP_RC         = (105, '8H',     False, Critical)
-    MSP_RAW_GPS    = (106, '2B2I3H', False, Inactive)
-    MSP_COMP_GPS   = (107, '2HB',    False, Inactive)
-    MSP_ATTITUDE   = (108, '3h',     False, High)
-    MSP_ALTITUDE   = (109, 'ih',     False, High)
-    MSP_ANALOG     = (110, 'B3H',    False, Inactive)
-    MSP_RC_TUNING  = (111, '7B',     False, Inactive)
-    MSP_PID        = (112, '30B',    False, Inactive)
-    MSP_BOX        = (113, 'H',      True,  Inactive)
-    MSP_MISC       = (114, '6HIH4B', False, Inactive)
-    MSP_MOTOR_PINS = (115, '8B',     False, Inactive)
-    MSP_BOXNAMES   = (116, 's',      True,  Inactive)
-    MSP_PIDNAMES   = (117, 's',      True,  Inactive)
-    MSP_WP         = (118, 'B3I2HB', False, Inactive)
-    MSP_BOXIDS     = (119, 'B',      True,  Inactive)
-    MSP_SERVO_CONF = (120, '3HB',    True,  Inactive)
-
-    # Set commands.
-    MSP_SET_RAW_RC      = (200, '8H',     False, Critical)
-    MSP_SET_RAW_GPS     = (201, '2B2I2H', False, Inactive)
-    MSP_SET_PID         = (202, '30B',    False, Inactive)
-    MSP_SET_BOX         = (203, 'H',      True,  Inactive)
-    MSP_SET_RC_TUNING   = (204, '7B',     False, Inactive)
-    MSP_ACC_CALIBRATION = (205, '',       False, Inactive)
-    MSP_MAG_CALIBRATION = (206, '',       False, Inactive)
-    MSP_SET_MISC        = (207, '6HIH4B', False, Inactive)
-    MSP_RESET_CONF      = (208, '',       False, Inactive)
-    MSP_SET_HEAD        = (211, 'h',      False, Inactive)
-    MSP_SET_SERVO_CONF  = (212, '56B',    False, Inactive)
-    MSP_SET_MOTOR       = (214, '8H',     False, Inactive)
-    MSP_BIND            = (240, '',       False, Inactive)
-    MSP_EEPROM_WRITE    = (250, '',       False, Inactive)
     """
 
     # ------------------------------------ CLASS VARIABLES -------------------------------------
