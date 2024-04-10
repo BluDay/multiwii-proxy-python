8236f6d35ee6e5b5f1220f597b40034d77188f5c
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-10 14:06:33 +0200

    feat: add back messaging

---
+++ wiiproxy/messaging/MultiWiiMessageDirection.py
@@ -0,0 +1,37 @@
+from typing import Final
+
+class MultiWiiMessageDirection(object):
+    """
+    An enum-like class for message direction characters. Includes additional methods
+    for retrieving each direction character as a serialized value.
+    """
+
+    """
+    The error character.
+    """
+    ERROR: Final[str] = '!'
+    
+    """
+    The incoming direction character.
+    """
+    INCOMING: Final[str] = '<'
+
+    """
+    The outgoing direction character.
+    """
+    OUTGOING: Final[str] = '>'
+    
+    """
+    A serialized error character.
+    """
+    SERIALIZED_ERROR: Final[int] = 0 # serialize_to_int8(ERROR)
+
+    """
+    A serialized incoming direction character.
+    """
+    SERIALIZED_INCOMING: Final[int] = 0 # serialize_to_int8(INCOMING)
+
+    """
+    A serialized outgoing direction character.
+    """
+    SERIALIZED_OUTGOING: Final[int] = 0 # serialize_to_int8(OUTGOING)
