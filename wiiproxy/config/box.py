2a2139a2af997bbce4c139cbf281b34aae88646e
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-11 23:28:56 +0200

    feat: convert config module to a directory with split files

---
+++ wiiproxy/config/box.py
@@ -0,0 +1,27 @@
+from enum import IntEnum, unique
+
+@unique
+class MultiWiiBox(IntEnum):
+    """Represents the various boxes that can be checked."""
+    Arm       = 0
+    Angle     = 1
+    Horizon   = 2
+    Baro      = 3
+    Vario     = 4
+    Mag       = 5
+    HeadFree  = 6
+    HeadAdj   = 7
+    CamStab   = 8
+    CamTrig   = 9
+    GpsHome   = 10
+    GpsHold   = 11
+    Passthru  = 12
+    Beeper    = 13
+    LedMax    = 14
+    LedLow    = 15
+    LLights   = 16
+    Calib     = 17
+    Governor  = 18
+    OsdSwitch = 19
+    Mission   = 20
+    Land      = 21
