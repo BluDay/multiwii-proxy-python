ce7b4dfa3983cf3cfa1a33746638608ab24570ca
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-11 23:28:56 +0200

    feat: convert config module to a directory with split files

---
+++ wiiproxy/config/multitype.py
@@ -0,0 +1,26 @@
+from enum import IntEnum, unique
+
+@unique
+class MultiWiiMultitype(IntEnum):
+    """Represents different types of available vehicle configurations."""
+    Unidentified = 0
+    Tri          = 1
+    QuadP        = 2
+    QuadX        = 3
+    Bi           = 4
+    Gimbal       = 5
+    Y6           = 6
+    Hex6         = 7
+    FlyingWing   = 8
+    Y4           = 9
+    Hex6X        = 10
+    OctoX8       = 11
+    OctoflatP    = 12
+    OctoflatX    = 13
+    Airplane     = 14
+    Heli120CCPM  = 15
+    Heli90Deg    = 16
+    VTail4       = 17
+    Hex6H        = 18
+    Singlecopter = 21
+    Dualcopter   = 20
