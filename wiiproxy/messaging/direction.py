b1cbe8c26191607676a5049751ba977ff3b1eb56
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-11 23:22:26 +0200

    feat: turn messaging submodule file to a directory and additional modules

---
+++ wiiproxy/messaging/direction.py
@@ -0,0 +1,22 @@
+from typing import Final
+
+class MultiWiiMessageDirection(object):
+    """An enum-like class for message direction characters."""
+
+    """The error character."""
+    ERROR: Final[str] = '!'
+    
+    """The incoming direction character."""
+    INCOMING: Final[str] = '<'
+
+    """The outgoing direction character."""
+    OUTGOING: Final[str] = '>'
+    
+    """A serialized error character."""
+    SERIALIZED_ERROR: Final[int] = int() # serialize_to_int8(ERROR)
+
+    """A serialized incoming direction character."""
+    SERIALIZED_INCOMING: Final[int] = int() # serialize_to_int8(INCOMING)
+
+    """A serialized outgoing direction character."""
+    SERIALIZED_OUTGOING: Final[int] = int() # serialize_to_int8(OUTGOING)
---
+++ wiiproxy/messaging/header.py
@@ -0,0 +1,22 @@
+from typing import Final
+
+class MultiWiiMessageHeader(object):
+    """The class for managing and analyzing MultiWii messages."""
+
+    """The fixed MSP v1 preamble used for all messages."""
+    PREAMBLE: Final[str] = '$M'
+
+    """The incoming header."""
+    INCOMING: Final[str] = f'{PREAMBLE}{MultiWiiMessageDirection.INCOMING}'
+
+    """The outgoing header."""
+    OUTGOING: Final[str] = f'{PREAMBLE}{MultiWiiMessageDirection.OUTGOING}'
+
+    """ A serialized preamble."""
+    SERIALIZED_PREAMBLE: Final[bytes] = int() # encode(PREAMBLE)
+
+    """A serialized incoming header."""
+    SERIALIZED_INCOMING: Final[bytes] = int() # encode(INCOMING)
+
+    """A serialized outgoing header."""
+    SERIALIZED_OUTGOING: Final[bytes] = int() # encode(OUTGOING)
