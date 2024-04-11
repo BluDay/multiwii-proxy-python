e1915c254c8692865ed4c09d78473ac0e111d93f
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-11 20:57:41 +0200

    feat: add back MultiWiiDataValues and create corresponding instance member

--- wiiproxy/data.py
+++ wiiproxy/data.py
@@ -1,4 +1,8 @@
-from wiiproxy.config import MultiWiiBox, MultiWiiCapability, MultiWiiMultitype
+from wiiproxy.config import (
+    MultiWiiBox,
+    MultiWiiCapability,
+    MultiWiiMultitype
+)
 
 from dataclasses import dataclass
 from typing      import Final, NoReturn
@@ -193,3 +197,54 @@ class Waypoint(_MultiWiiDataStructure):
     time_to_stay: Final[int | None]
 
     flag: Final[int | None]
+
+@dataclass(slots=True)
+class MultiWiiDataValues(object):
+    """Represents a collection of data values for all MultiWii structures."""
+    ident:      Ident
+    status:     Status
+    raw_imu:    RawImu
+    servo:      Servo
+    servo_conf: ServoConf
+    motor:      Motor
+    motor_pins: MotorPins
+    rc:         Rc
+    rc_tuning:  RcTuning
+    attitude:   Attitude
+    altitude:   Altitude
+    raw_gps:    RawGps
+    comp_gps:   CompGps
+    wp:         Waypoint
+    analog:     Analog
+    pid:        Pid
+    pidnames:   PidNames
+    box:        Box
+    boxnames:   BoxNames
+    boxids:     BoxIds
+    misc:       Misc
+
+    # ------------------------------------ INSTANCE METHODS ------------------------------------
+
+    def reset_data(self) -> NoReturn:
+        """Resets all data value instances. defines each field if not already defined."""
+        self.ident      = Ident()
+        self.status     = Status()
+        self.raw_imu    = RawImu()
+        self.servo      = Servo()
+        self.servo_conf = ServoConf()
+        self.motor      = Motor()
+        self.motor_pins = MotorPins()
+        self.rc         = Rc()
+        self.rc_tuning  = RcTuning()
+        self.attitude   = Attitude()
+        self.altitude   = Altitude()
+        self.raw_gps    = RawGps()
+        self.comp_gps   = CompGps()
+        self.wp         = Waypoint()
+        self.analog     = Analog()
+        self.pid        = Pid()
+        self.pidnames   = PidNames()
+        self.box        = Box()
+        self.boxnames   = BoxNames()
+        self.boxids     = BoxIds()
+        self.misc       = Misc()
