aacb12068bb03dd88bda9f791c7ccaca15e21675
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-12 15:00:11 +0200

    feat: add untracked files, update class name and fix __all__

--- wiiproxy/data/_values.py
+++ wiiproxy/data/_values.py
@@ -1,32 +1,30 @@
-from wiiproxy.data import (
-    Altitude,
-    Analog,
-    Attitude,
-    Box,
-    BoxIds,
-    BoxNames,
-    CompGps,
-    Ident,
-    Misc,
-    Motor,
-    MotorPins,
-    Pid,
-    PidNames,
-    RawGps,
-    RawImu,
-    Rc,
-    RcTuning,
-    Servo,
-    ServoConf,
-    Status,
-    Waypoint
-)
+from wiiproxy.data.altitude   import Altitude
+from wiiproxy.data.analog     import Analog
+from wiiproxy.data.attitude   import Attitude
+from wiiproxy.data.box        import Box
+from wiiproxy.data.box_ids    import BoxIds
+from wiiproxy.data.box_names  import BoxNames
+from wiiproxy.data.comp_gps   import CompGps
+from wiiproxy.data.ident      import Ident
+from wiiproxy.data.misc       import Misc
+from wiiproxy.data.motor      import Motor
+from wiiproxy.data.motor_pins import MotorPins
+from wiiproxy.data.pid        import Pid
+from wiiproxy.data.pid_names  import PidNames
+from wiiproxy.data.raw_gps    import RawGps
+from wiiproxy.data.raw_imu    import RawImu
+from wiiproxy.data.rc         import Rc
+from wiiproxy.data.rc_tuning  import RcTuning
+from wiiproxy.data.servo      import Servo
+from wiiproxy.data.servo_conf import ServoConf
+from wiiproxy.data.status     import Status
+from wiiproxy.data.waypoint   import Waypoint
 
 from dataclasses import dataclass
 from typing      import NoReturn
 
 @dataclass(slots=True)
-class MultiWiiDataValues(object):
+class _MultiWiiDataValues(object):
     """Represents a collection of data values for all MultiWii structures."""
     
     # ---------------------------------- INSTANCE VARIABLES ------------------------------------
