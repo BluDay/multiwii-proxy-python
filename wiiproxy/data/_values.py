df5fa0a002bdb271bffdfa19918fbc6a5c00a089
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-15 15:53:24 +0200

    feat: remove old implementation for all property getters in _values

--- wiiproxy/data/_values.py
+++ wiiproxy/data/_values.py
@@ -27,131 +27,131 @@ class MultiWiiDataValues(object):
     
     # ---------------------------------- INSTANCE VARIABLES ------------------------------------
 
-    ident:      Ident
-    status:     Status
-    raw_imu:    RawImu
-    servo:      Servo
-    servo_conf: ServoConf
-    motor:      Motor
-    motor_pins: MotorPins
-    rc:         Rc
-    rc_tuning:  RcTuning
-    attitude:   Attitude
-    altitude:   Altitude
-    raw_gps:    RawGps
-    comp_gps:   CompGps
-    wp:         Waypoint
-    analog:     Analog
-    pid:        Pid
-    pidnames:   PidNames
-    box:        Box
-    boxnames:   BoxNames
-    boxids:     BoxIds
-    misc:       Misc
+    _ident:      Ident
+    _status:     Status
+    _raw_imu:    RawImu
+    _servo:      Servo
+    _servo_conf: ServoConf
+    _motor:      Motor
+    _motor_pins: MotorPins
+    _rc:         Rc
+    _rc_tuning:  RcTuning
+    _attitude:   Attitude
+    _altitude:   Altitude
+    _raw_gps:    RawGps
+    _comp_gps:   CompGps
+    _wp:         Waypoint
+    _analog:     Analog
+    _pid:        Pid
+    _pidnames:   PidNames
+    _box:        Box
+    _boxnames:   BoxNames
+    _boxids:     BoxIds
+    _misc:       Misc
 
     # --------------------------------------- PROPERTIES ---------------------------------------
 
     @property
     def ident(self) -> Ident:
         """Deserializes the retrieved MSP_IDENT message."""
-        return self._deserialize(MspCommands.IDENT, Ident)
+        return self._ident
 
     @property
     def status(self) -> Status:
         """Deserializes the retrieved MSP_STATUS message."""
-        return self._deserialize(MspCommands.STATUS, Status)
+        return self._status
 
     @property
     def raw_imu(self) -> RawImu:
         """Deserializes the retrieved MSP_RAW_IMU message."""
-        return self._deserialize(MspCommands.RAW_IMU, RawImu)
+        return self._raw_imu
 
     @property
     def servo(self) -> Servo:
         """Deserializes the retrieved MSP_SERVO message."""
-        return self._deserialize(MspCommands.SERVO, Servo)
+        return self._servo
 
     @property
     def servo_conf(self) -> ServoConf:
         """Deserializes the retrieved MSP_SERVO_CONF message."""
-        return self._deserialize(MspCommands.SERVO_CONF, ServoConf)
+        return self._servo_conf
 
     @property
     def motor(self) -> Motor:
         """Deserializes the retrieved MSP_MOTOR message."""
-        return self._deserialize(MspCommands.MOTOR, Motor)
+        return self._motor
 
     @property
     def motor_pins(self) -> MotorPins:
         """Deserializes the retrieved MSP_MOTOR_PINS message."""
-        return self._deserialize(MspCommands.MOTOR_PINS, MotorPins)
+        return self._motor_pins
 
     @property
     def rc(self) -> Rc:
         """Deserializes the retrieved MSP_RC message."""
-        return self._deserialize(MspCommands.RC, Rc)
+        return self._rc
 
     @property
     def rc_tuning(self) -> RcTuning:
         """Deserializes the retrieved MSP_RC_TUNING message."""
-        return self._deserialize(MspCommands.RC_TUNING, RcTuning)
+        return self._rc_tuning
     
     @property
     def attitude(self) -> Attitude:
         """Deserializes the retrieved MSP_ATTITUDE message."""
-        return self._deserialize(MspCommands.ATTITUDE, Attitude)
+        return self._attitude
 
     @property
     def altitude(self) -> Altitude:
         """Deserializes the retrieved MSP_ALTITUDE message."""
-        return self._deserialize(MspCommands.ALTITUDE, Altitude)
+        return self._altitude
 
     @property
     def raw_gps(self) -> RawGps:
         """Deserializes the retrieved MSP_RAW_GPS message."""
-        return self._deserialize(MspCommands.RAW_GPS, RawGps)
+        return self._raw_gps
 
     @property
     def comp_gps(self) -> CompGps:
         """Deserializes the retrieved MSP_COMP_GPS message."""
-        return self._deserialize(MspCommands.COMP_GPS, CompGps)
+        return self._comp_gps
 
     @property
     def wp(self) -> Waypoint:
         """Deserializes the retrieved MSP_WP message."""
-        return self._deserialize(MspCommands.WP, Waypoint)
+        return self._wp
 
     @property
     def analog(self) -> Analog:
         """Deserializes the retrieved MSP_ANALOG message."""
-        return self._deserialize(MspCommands.ANALOG, Analog)
+        return self._analog
 
     @property
     def pid(self) -> Pid:
         """Deserializes the retrieved MSP_PID message."""
-        return self._deserialize(MspCommands.PID, Pid)
+        return self._pid
 
     @property
     def pidnames(self) -> PidNames:
         """Deserializes the retrieved MSP_PIDNAMES message."""
-        return self._deserialize(MspCommands.PIDNAMES, PidNames)
+        return self._pidnames
 
     @property
     def box(self) -> Box:
         """Deserializes the retrieved MSP_BOX message."""
-        return self._deserialize(MspCommands.BOX, Box)
+        return self._box
 
     @property
     def boxnames(self) -> BoxNames:
         """Deserializes the retrieved MSP_BOXNAMES message."""
-        return self._deserialize(MspCommands.BOXNAMES, BoxNames)
+        return self._boxnames
 
     @property
     def boxids(self) -> BoxIds:
         """Deserializes the retrieved MSP_BOXIDS message."""
-        return self._deserialize(MspCommands.BOXIDS, BoxIds)
+        return self._boxids
 
     @property
     def misc(self) -> Misc:
         """Deserializes the retrieved MSP_MISC message."""
-        return self._deserialize(MspCommands.MISC, Misc)
+        return self._misc
