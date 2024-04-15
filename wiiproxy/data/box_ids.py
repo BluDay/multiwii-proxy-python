4ab8dceb3f126b88ab60751b063517579ef8f6d5
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-15 14:41:53 +0200

    feat: specify new boolean for variable-sized structures and add back commands file

--- wiiproxy/data/box_ids.py
+++ wiiproxy/data/box_ids.py
@@ -3,7 +3,7 @@ from ._base import msp_command_code, msp_data_struct_format, MultiWiiDataStructu
 from ..config import MultiWiiBox
 
 @msp_command_code(119)
-@msp_data_struct_format('B')
+@msp_data_struct_format('B', has_variable_size=True)
 class BoxIds(MultiWiiDataStructure):
     """Represents data values for the MSP_BOXIDS command."""
     pass
--- wiiproxy/data/box_names.py
+++ wiiproxy/data/box_names.py
@@ -1,7 +1,7 @@
 from ._base import msp_command_code, msp_data_struct_format, MultiWiiDataStructure
 
 @msp_command_code(116)
-@msp_data_struct_format('s')
+@msp_data_struct_format('s', has_variable_size=True)
 class BoxNames(MultiWiiDataStructure):
     """Represents data values for the MSP_BOXNAMES command."""
     pass
