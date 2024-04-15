b987cafdd0da6608b2f15dd5099925b51b67c236
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-15 23:45:24 +0200

    feat: add more project info

--- wiiproxy/__init__.py
+++ wiiproxy/__init__.py
@@ -1,9 +1,13 @@
 """( 0 _ o )"""
 
-__author__  = 'BluDay'
-
-__license__ = 'MIT'
-
-__version__ = '1.0'
+__author__      = 'BluDay'
+__copyrights__  = '© 2024 BluDay'
+__credits__     = 'BluDay'
+__description__ = 'A user-friendly and multithreaded Python 3 module for controlling MultiWii-based drones.'
+__license__     = 'MIT'
+__maintainer__  = 'BluDay'
+__title__       = 'wiiproxy'
+__url__         = 'https://github.com/BluDay/wiiproxy'
+__version__     = '1.0'
 
 from .core import MultiWii
