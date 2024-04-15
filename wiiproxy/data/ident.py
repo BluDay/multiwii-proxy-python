04ae8a47a88f26dc0bcde9f7c37492f195bae346
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-15 16:06:00 +0200

    feat: use new overridden _evaluate_data method in Ident

--- wiiproxy/data/ident.py
+++ wiiproxy/data/ident.py
@@ -53,12 +53,10 @@ class Ident(MultiWiiDataStructure):
 
     # ----------------------------------- INSTANCE METHODS -------------------------------------
 
-    def _update(self, data: bytes) -> NoReturn:
-        """Updates the current values by the provided unserialized data bytes.
+    def _evaluate_data(self) -> NoReturn:
+        """Updates the current values by the provided unserialized data bytes."""
+        data = self._raw_data
 
-        Parameters:
-            data (bytes): A buffer with the raw data values.
-        """
         self._version = data[0] / 100
 
         self._multitype = MultiWiiMultitype(data[1])
