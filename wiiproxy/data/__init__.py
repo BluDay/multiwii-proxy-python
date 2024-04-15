fb12580313ec67abe0f09ca0f8fb1dd39c469288
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-15 13:25:43 +0200

    feat: add missing decorators for Point classes

--- wiiproxy/data/__init__.py
+++ wiiproxy/data/__init__.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from typing      import NamedTuple
 
+@dataclass
 class Point2D(NamedTuple):
     """A tuple of x and y integer values for a 2D point."""
     x: int = 0
     y: int = 0
 
+@dataclass
 class Point3D(NamedTuple):
     """A tuple of x, y and z integer values for a 3D point."""
     x: int = 0
