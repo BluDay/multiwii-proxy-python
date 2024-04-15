bac6af7cea70c9e06a3aa6488805d20ca9d30826
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-15 12:41:36 +0200

    feat: rehaul base data classes and remove abstract collection classes

--- wiiproxy/data/_base.py
+++ wiiproxy/data/_base.py
@@ -1,21 +1,18 @@
-from abc         import ABC, abstractmethod
-from dataclasses import dataclass
-from typing      import NoReturn, Optional
+from collections.abc import ABC, abstractmethod
+from typing          import Final, NoReturn
 
-class _MultiWiiDataStructure(object):
+class _MultiWiiDataStructure(ABC):
     """Represents the base class for MSP data structure classes."""
+
+    # ----------------------------------- CLASS CONSTANTS --------------------------------------
+
+    COMMAND_CODE: Final[int]
+
+    STRUCT_FORMAT: Final[str]
+
+    # ----------------------------------- INSTANCE METHODS -------------------------------------
     
     @abstractmethod
     def _update(self, data: bytes) -> NoReturn:
         """Deserialize bytes to a derived data structure type."""
         pass
-
-@dataclass(slots=True)
-class _MultiWiiDataIntegerValues(_MultiWiiDataStructure):
-    """The base MultiWii class for data values with a single, public int tuple member."""
-    values: Optional[tuple[int]]
-
-@dataclass(slots=True)
-class _MultiWiiDataStringValues(_MultiWiiDataStructure):
-    """The base class for data values with a single, public string tuple member."""
-    values: Optional[tuple[str]]
