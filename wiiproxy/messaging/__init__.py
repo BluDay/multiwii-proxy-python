585b5036ad928a515d280f823f23d374766d48ad
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-15 22:47:12 +0200

    feat: add rename message direction character class and file

--- wiiproxy/messaging/__init__.py
+++ wiiproxy/messaging/__init__.py
@@ -1,8 +1,8 @@
 from .msp_commands import MspCommands
 
-from .msp_message_direction import MspMessageDirection
-from .msp_message_header    import MspMessageHeader
-from .msp_message_payload   import MspMessagePayload
+from .msp_message_direction_character import MspMessageDirectionCharacter
+from .msp_message_header              import MspMessageHeader
+from .msp_message_payload             import MspMessagePayload
 
 @staticmethod
 def get_command_codes() -> tuple[int]:
