b77abb45e974f57354c1354aa029ad9478a9eb83
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-17 14:41:53 +0200

    feat: update package title casing and readme file

--- wiiproxy/__init__.py
+++ wiiproxy/__init__.py
@@ -6,7 +6,7 @@ __credits__     = 'BluDay'
 __description__ = 'A user-friendly and multithreaded Python 3 module for controlling MultiWii-based drones.'
 __license__     = 'MIT'
 __maintainer__  = 'BluDay'
-__title__       = 'wiiproxy'
+__title__       = 'WiiProxy'
 __url__         = 'https://github.com/BluDay/wiiproxy'
 __version__     = '1.0'
 
