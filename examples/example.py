9e83d1f2a2a1f148462cfab80ac38332a81796ad
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-30 13:48:02 +0200

    feat: update example script

--- examples/example.py
+++ examples/example.py
@@ -5,15 +5,15 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from wiiproxy.data      import Ident, RawImu
+from wiiproxy.data      import MspIdent, MspRawImu
 from wiiproxy.messaging import MspMessageHeader
 
 print(f'Header preamble: "{MspMessageHeader.PREAMBLE}" ({MspMessageHeader.SERIALIZED_PREAMBLE})')
 print(f'Incoming header: "{MspMessageHeader.INCOMING}" ({MspMessageHeader.SERIALIZED_INCOMING})')
 print(f'Outgoing header: "{MspMessageHeader.OUTGOING}" ({MspMessageHeader.SERIALIZED_OUTGOING})')
 
-print(f'MSP_IDENT ({Ident.COMMAND_CODE})')
-print(f'MSP_RAW_IMU ({RawImu.COMMAND_CODE})')
+print(f'MSP_IDENT ({MspIdent.COMMAND_CODE})')
+print(f'MSP_RAW_IMU ({MspRawImu.COMMAND_CODE})')
 
 """
 from serial   import Serial
