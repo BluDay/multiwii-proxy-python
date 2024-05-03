2f069b18cb90ddb35f8febca536b96f49a245c1b
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-03 11:00:41 +0200

    feat: remove all structure data states and add propery ctor comment

--- wiiproxy/multiwii.py
+++ wiiproxy/multiwii.py
@@ -84,28 +84,6 @@ class MultiWii(object):
 
     # ---------------------------------- INSTANCE VARIABLES ------------------------------------
 
-    _altitude:   MspAltitude
-    _analog:     MspAnalog
-    _attitude:   MspAttitude
-    _box:        MspBox
-    _box_ids:    MspBoxIds
-    _box_names:  MspBoxNames
-    _comp_gps:   MspCompGps
-    _ident:      MspIdent
-    _misc:       MspMisc
-    _motor:      MspMotor
-    _motor_pins: MspMotorPins
-    _pid:        MspPid
-    _pid_names:  MspPidNames
-    _raw_gps:    MspRawGps
-    _raw_imu:    MspRawImu
-    _rc:         MspRc
-    _rc_tuning:  MspRcTuning
-    _servo:      MspServo
-    _servo_conf: MspServoConf
-    _status:     MspStatus
-    _waypoint:   MspWaypoint
-
     _command_write_read_delay: int
 
     _serial: Serial
@@ -113,7 +91,14 @@ class MultiWii(object):
     # ------------------------------------- MAGIC METHODS --------------------------------------
 
     def __init__(self, serial: Serial) -> NoReturn:
-        """Initializes an instance using the provided serial port."""
+        """Initializes an instance using the provided serial port.
+
+        This constructor initializes a new instance of the MultiWii class using the provided
+        serial port for communication with the FC. It sets up the initial state of the object,
+        including the activation status, command write-read delay and serial port configuration.
+        Additionally, it ensures that the provided serial port is of the correct type (Serial).
+        If the serial port is not of the expected type, a TypeError is raised.
+        """
         if not isinstance(serial, Serial):
             raise TypeError('"serial" must be an instance of "Serial".')
 
@@ -123,114 +108,7 @@ class MultiWii(object):
 
         self._serial = serial
 
-        self.reset_data_values()
-
     # --------------------------------------- PROPERTIES ---------------------------------------
-
-    @property
-    def altitude(self) -> MspAltitude:
-        """Sends the MSP_ALTITUDE command and gets the data instance."""
-        return self._altitude
-
-    @property
-    def analog(self) -> MspAnalog:
-        """Sends the MSP_ANALOG command and gets the data instance."""
-        return self._analog
-
-    @property
-    def attitude(self) -> MspAttitude:
-        """Sends the MSP_ATTITUDE command and gets the data instance."""
-        return self._attitude
-    
-    @property
-    def box(self) -> MspBox:
-        """Sends the MSP_BOX command and gets the data instance."""
-        return self._box
-
-    @property
-    def boxids(self) -> MspBoxIds:
-        """Sends the MSP_BOXIDS command and gets the data instance."""
-        return self._box_ids
-
-    @property
-    def boxnames(self) -> MspBoxNames:
-        """Sends the MSP_BOXNAMES command and gets the data instance."""
-        return self._box_names
-    
-    @property
-    def comp_gps(self) -> MspCompGps:
-        """Sends the MSP_COMP_GPS command and gets the data instance."""
-        return self._comp_gps
-
-    @property
-    def ident(self) -> MspIdent:
-        """Sends the MSP_IDENT command and gets the data instance."""
-        return self._ident
-
-    @property
-    def misc(self) -> MspMisc:
-        """Sends the MSP_MISC command and gets the data instance."""
-        return self._misc
-
-    @property
-    def motor(self) -> MspMotor:
-        """Sends the MSP_MOTOR command and gets the data instance."""
-        return self._motor
-
-    @property
-    def motor_pins(self) -> MspMotorPins:
-        """Sends the MSP_MOTOR_PINS command and gets the data instance."""
-        return self._motor_pins
-    
-    @property
-    def pid(self) -> MspPid:
-        """Sends the MSP_PID command and gets the data instance."""
-        return self._pid
-
-    @property
-    def pidnames(self) -> MspPidNames:
-        """Sends the MSP_PIDNAMES command and gets the data instance."""
-        return self._pid_names
-
-    @property
-    def raw_gps(self) -> MspRawGps:
-        """Sends the MSP_RAW_GPS command and gets the data instance."""
-        return self._raw_gps
-
-    @property
-    def raw_imu(self) -> MspRawImu:
-        """Sends the MSP_RAW_IMU command and gets the data instance."""
-        return self._raw_imu
-
-    @property
-    def rc(self) -> MspRc:
-        """Sends the MSP_RC command and gets the data instance."""
-        return self._rc
-
-    @property
-    def rc_tuning(self) -> MspRcTuning:
-        """Sends the MSP_RC_TUNING command and gets the data instance."""
-        return self._rc_tuning
-
-    @property
-    def servo(self) -> MspServo:
-        """Sends the MSP_SERVO command and gets the data instance."""
-        return self._servo
-
-    @property
-    def servo_conf(self) -> MspServoConf:
-        """Sends the MSP_SERVO_CONF command and gets the data instance."""
-        return self._servo_conf
-
-    @property
-    def status(self) -> MspStatus:
-        """Sends the MSP_STATUS command and gets the data instance."""
-        return self._status
-
-    @property
-    def waypoint(self) -> MspWaypoint:
-        """Sends the MSP_WP command and gets the data instance."""
-        return self._waypoint
 
     @property
     def command_write_read_delay(self) -> float:
@@ -298,29 +176,91 @@ class MultiWii(object):
         """Sends a message of a specified command and data values to the FC."""
         pass
 
-    def reset_data_values(self) -> NoReturn:
-        """Resets all data values."""
-        self._altitude   = MspAltitude()
-        self._analog     = MspAnalog()
-        self._attitude   = MspAttitude()
-        self._box        = MspBox()
-        self._box_ids    = MspBoxIds()
-        self._box_names  = MspBoxNames()
-        self._comp_gps   = MspCompGps()
-        self._ident      = MspIdent()
-        self._misc       = MspMisc()
-        self._motor      = MspMotor()
-        self._motor_pins = MspMotorPins()
-        self._pid        = MspPid()
-        self._pid_names  = MspPidNames()
-        self._raw_gps    = MspRawGps()
-        self._raw_imu    = MspRawImu()
-        self._rc         = MspRc()
-        self._rc_tuning  = MspRcTuning()
-        self._servo      = MspServo()
-        self._servo_conf = MspServoConf()
-        self._status     = MspStatus()
-        self._waypoint   = MspWaypoint()
+    # ------------------------------------- GET COMMANDS ---------------------------------------
+
+    def get_altitude(self) -> MspAltitude:
+        """Sends the MSP_ALTITUDE command and gets the data instance."""
+        return None
+    
+    def get_analog(self) -> MspAnalog:
+        """Sends the MSP_ANALOG command and gets the data instance."""
+        return None
+    
+    def get_attitude(self) -> MspAttitude:
+        """Sends the MSP_ATTITUDE command and gets the data instance."""
+        return None
+    
+    def get_box(self) -> MspBox:
+        """Sends the MSP_BOX command and gets the data instance."""
+        return None
+    
+    def get_boxids(self) -> MspBoxIds:
+        """Sends the MSP_BOXIDS command and gets the data instance."""
+        return None
+    
+    def get_boxnames(self) -> MspBoxNames:
+        """Sends the MSP_BOXNAMES command and gets the data instance."""
+        return None
+    
+    def get_comp_gps(self) -> MspCompGps:
+        """Sends the MSP_COMP_GPS command and gets the data instance."""
+        return None
+    
+    def get_ident(self) -> MspIdent:
+        """Sends the MSP_IDENT command and gets the data instance."""
+        return None
+    
+    def get_misc(self) -> MspMisc:
+        """Sends the MSP_MISC command and gets the data instance."""
+        return None
+    
+    def get_motor(self) -> MspMotor:
+        """Sends the MSP_MOTOR command and gets the data instance."""
+        return None
+    
+    def get_motor_pins(self) -> MspMotorPins:
+        """Sends the MSP_MOTOR_PINS command and gets the data instance."""
+        return None
+    
+    def get_pid(self) -> MspPid:
+        """Sends the MSP_PID command and gets the data instance."""
+        return None
+    
+    def get_pidnames(self) -> MspPidNames:
+        """Sends the MSP_PIDNAMES command and gets the data instance."""
+        return None
+    
+    def get_raw_gps(self) -> MspRawGps:
+        """Sends the MSP_RAW_GPS command and gets the data instance."""
+        return None
+    
+    def get_raw_imu(self) -> MspRawImu:
+        """Sends the MSP_RAW_IMU command and gets the data instance."""
+        return None
+    
+    def get_rc(self) -> MspRc:
+        """Sends the MSP_RC command and gets the data instance."""
+        return None
+    
+    def get_rc_tuning(self) -> MspRcTuning:
+        """Sends the MSP_RC_TUNING command and gets the data instance."""
+        return None
+    
+    def get_servo(self) -> MspServo:
+        """Sends the MSP_SERVO command and gets the data instance."""
+        return None
+    
+    def get_servo_conf(self) -> MspServoConf:
+        """Sends the MSP_SERVO_CONF command and gets the data instance."""
+        return None
+    
+    def get_status(self) -> MspStatus:
+        """Sends the MSP_STATUS command and gets the data instance."""
+        return None
+    
+    def get_waypoint(self) -> MspWaypoint:
+        """Sends the MSP_WP command and gets the data instance."""
+        return None
 
     # ------------------------------------- SET COMMANDS ---------------------------------------
 
