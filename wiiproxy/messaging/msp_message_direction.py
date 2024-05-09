161c230c4d56ca82730df2a2a6ec8799121abf43
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-09 19:22:39 +0200

    feat: remove self-explanatory comments and improve comments

--- wiiproxy/messaging/msp_message_direction.py
+++ wiiproxy/messaging/msp_message_direction.py
@@ -1,22 +1,17 @@
+from struct import pack
 from typing import Final
 
 class MspMessageDirection(object):
-    """An enum-like class for message direction characters."""
+    """An enum-like class for MSP message direction characters."""
 
-    """The error character."""
     ERROR_CHAR: Final[str] = '!'
     
-    """The incoming direction character."""
     INCOMING_CHAR: Final[str] = '<'
 
-    """The outgoing direction character."""
     OUTGOING_CHAR: Final[str] = '>'
     
-    """A serialized error character."""
     SERIALIZED_ERROR_CHAR: Final[int] = None
 
-    """A serialized incoming direction character."""
     SERIALIZED_INCOMING_CHAR: Final[int] = None
 
-    """A serialized outgoing direction character."""
     SERIALIZED_OUTGOING_CHAR: Final[int] = None
