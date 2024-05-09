d3ad3c728152a920456cf9907908ad2d7dd47ee2
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-09 19:22:39 +0200

    feat: remove self-explanatory comments and improve comments

--- wiiproxy/messaging/msp_message_header.py
+++ wiiproxy/messaging/msp_message_header.py
@@ -3,22 +3,16 @@ from . import MspMessageDirection
 from typing import Final
 
 class MspMessageHeader(object):
-    """The class for managing and analyzing MultiWii messages."""
+    """An enum-like class for pre-built MSP message headers."""
 
-    """The fixed MSP v1 preamble used for all messages."""
     PREAMBLE: Final[str] = '$M'
 
-    """The incoming header."""
     INCOMING: Final[str] = PREAMBLE + MspMessageDirection.INCOMING_CHAR
 
-    """The outgoing header."""
     OUTGOING: Final[str] = PREAMBLE + MspMessageDirection.OUTGOING_CHAR
 
-    """A serialized preamble."""
     SERIALIZED_PREAMBLE: Final[bytes] = None
 
-    """A serialized incoming header."""
     SERIALIZED_INCOMING: Final[bytes] = None
 
-    """A serialized outgoing header."""
     SERIALIZED_OUTGOING: Final[bytes] = None
