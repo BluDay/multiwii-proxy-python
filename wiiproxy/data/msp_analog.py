2482ad55de974879d52936c3ebbcc2797edf007f
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-09 21:41:09 +0200

    feat: remove raw_data member and rename abstract method

--- wiiproxy/data/msp_analog.py
+++ wiiproxy/data/msp_analog.py
@@ -52,6 +52,6 @@ class MspAnalog(_MspDataStructure):
 
     # ----------------------------------- INSTANCE METHODS -------------------------------------
 
-    def _update_values(self, raw_data: bytes) -> NoReturn:
-        """Updates the current values by the provided unserialized data bytes."""
+    def _update(self, data: tuple) -> NoReturn:
+        """Updates the current values by unserialized data values."""
         pass
