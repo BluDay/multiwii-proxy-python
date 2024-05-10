ef39d67f83e02a0732d0e303f4a737452435a921
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-10 15:54:21 +0200

    feat: rehaul ident and altitude classes and add capabilities method

--- wiiproxy/data/msp_ident.py
+++ wiiproxy/data/msp_ident.py
@@ -3,68 +3,29 @@ from . import _MspDataStructure, command_code, struct_format
 from ..config       import MultiWiiCapability, MultiWiiMultitype
 from ..msp_commands import MSP_IDENT
 
-from typing import NoReturn
+from typing import Self
 
 @command_code(MSP_IDENT)
 @struct_format('3BI')
 class MspIdent(_MspDataStructure):
     """Represents data values for the MSP_IDENT command."""
-    
-    # ---------------------------------- INSTANCE VARIABLES ------------------------------------
+    version: float
 
-    _version: float
+    multitype: MultiWiiMultitype
 
-    _multitype: MultiWiiMultitype
+    capabilities: tuple[MultiWiiCapability]
 
-    _capabilities: tuple[MultiWiiCapability]
+    navi_version: int
 
-    _navi_version: int
-
-    # ------------------------------------- MAGIC METHODS --------------------------------------
-
-    def __init__(self) -> NoReturn:
-        """Initializes a new instance with default values."""
-        self._version      = None
-        self._multitype    = None
-        self._capabilities = None
-        self._navi_version = None
-
-    # -------------------------------------- PROPERTIES ----------------------------------------
-
-    @property
-    def version(self) -> float:
-        """Gets the MultiWii version."""
-        return self._version
-
-    @property
-    def multitype(self) -> MultiWiiMultitype:
-        """Gets the configured vehicle type."""
-        return self._multitype
-
-    @property
-    def capabilities(self) -> tuple[MultiWiiCapability]:
-        """Gets configured capabilities."""
-        return self._capabilities
-
-    @property
-    def navi_version(self) -> int:
-        """Gets the NAVI_VERSION."""
-        return self._navi_version
-
-    # ----------------------------------- INSTANCE METHODS -------------------------------------
-
-    def _update(self, data: tuple) -> NoReturn:
+    @staticmethod
+    def parse(data: tuple) -> Self:
         """Updates the current values by unserialized data values."""
-        self._version = data[0] / 100
+        version = data[0] / 100
 
-        self._multitype = MultiWiiMultitype(data[1])
+        multitype = MultiWiiMultitype(data[1])
 
-        capabilities = ()
+        capabilities = MultiWiiCapability.get_capabilities(data[2])
 
-        for capability in Capability:
-            if capability & data[2]:
-                capabilities += (capability,)
+        navi_version = data[3] << 28
 
-        self._capabilities = capabilities
-
-        self._navi_version = data[3] << 28
+        return MspIdent(version, multitype, capabilities, navi_version)
