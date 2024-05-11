f97595c6ab22e13c485a7fb825b11ef7a5757fb5
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-11 22:40:20 +0200

    feat: move all rehauled data structure classes to new submodule

--- wiiproxy/data/msp_motor_pins.py
+++
@@ -1,9 +0,0 @@
-from . import _MspValues, command_code, struct_format
-
-from ..msp_commands import MSP_MOTOR_PINS
-
-@command_code(MSP_MOTOR_PINS)
-@struct_format('8B')
-class MspMotorPins(_MspValues):
-    """Represents data values for the MSP_MOTOR_PINS command."""
-    pass
---
+++ wiiproxy/msp_data.py
@@ -0,0 +1,331 @@
+from .config import MultiWiiBoxState, MultiWiiMultitype, MultiWiiCapability
+
+from dataclasses import dataclass
+from typing      import Generic, NamedTuple, TypeVar
+
+T = TypeVar('T')
+
+@dataclass
+class _Point2D(NamedTuple, Generic[T]):
+    """A tuple of x and y integer values for a 2D point."""
+    x: T
+    y: T
+
+@dataclass
+class _Point3D(NamedTuple, Generic[T]):
+    """A tuple of x, y and z integer values for a 3D point."""
+    x: T
+    y: T
+    z: T
+
+class MspAltitude(object):
+    """Represents data values for the MSP_ALTITUDE command."""
+    estimation: int
+
+    pressure_variation: int
+
+class MspAnalog(object):
+    """Represents data values for the MSP_ANALOG command."""
+    voltage: int
+
+    power_meter_sum: int
+
+    rssi: int
+
+    amperage: int
+
+class MspAttitude(object):
+    """Represents data values for the MSP_ATTITUDE command."""
+    angle: _Point2D[float]
+
+    heading: int
+
+class MspBox(object):
+    """Represents data values for the MSP_BOX command."""
+    values: tuple[int]
+
+class MspSetBoxItem(object):
+    """Represents  for the MSP_SET_BOX command."""
+    aux1: MultiWiiBoxState
+    aux2: MultiWiiBoxState
+    aux3: MultiWiiBoxState
+    aux4: MultiWiiBoxState
+     
+    def compile(self) -> int:
+        """Compiles all of the set box state values to a single unsigned integer value."""
+        pass
+
+class MspBoxIds(object):
+    """Represents data values for the MSP_BOXIDS command."""
+    values: tuple[int]
+
+class MspBoxNames(object):
+    """Represents data values for the MSP_BOXNAMES command."""
+    names: tuple[str]
+
+class MspCompGps(object):
+    """Represents data values for the MSP_COMP_GPS command."""
+    distance_to_home: int
+
+    direction_to_home: int
+
+    update: int
+
+class MspIdent(object):
+    """Represents data values for the MSP_IDENT command."""
+    version: float
+
+    multitype: MultiWiiMultitype
+
+    capabilities: tuple[MultiWiiCapability]
+
+    navi_version: int
+
+class MspMisc(object):
+    """Represents data values for the MSP_MISC command."""
+    power_trigger: int
+
+    throttle_failsafe: int
+
+    throttle_idle: int
+
+    throttle_min: int
+
+    throttle_max: int
+
+    plog_arm: int
+
+    plog_lifetime: int
+
+    mag_declination: int
+
+    battery_scale: int
+
+    battery_warn_1: int
+
+    battery_warn_2: int
+
+    battery_critical: int
+
+class MspSetMisc(NamedTuple):
+    """Represents data values for the MSP_MISC command."""
+    power_trigger: int
+
+    throttle_min: int
+
+    throttle_max: int
+
+    min_command: int
+
+    failsafe_throttle: int
+
+    plog_arm: int
+
+    plog_lifetime: int
+
+    mag_declination: int
+
+    battery_scale: int
+
+    battery_warn_1: int
+
+    battery_warn_2: int
+
+    battery_critical: int
+
+class MspMotor(object):
+    """Represents data values for the MSP_MOTOR command."""
+    pass
+
+class MspSetMotor(NamedTuple):
+    """Data values as a tuple for the MSP_SET_MOTOR command."""
+    motor1: int
+    motor2: int
+    motor3: int
+    motor4: int
+    motor5: int
+    motor6: int
+    motor7: int
+    motor8: int
+
+class MspMotorPins(object):
+    """Represents data values for the MSP_MOTOR_PINS command."""
+    pass
+
+class MspPid(object):
+    """Represents data values for the MSP_PID command."""
+    roll_p: int
+    roll_i: int
+    roll_d: int
+
+    pitch_p: int
+    pitch_i: int
+    pitch_d: int
+
+    yaw_p: int
+    yaw_i: int
+    yaw_d: int
+
+    alt_p: int
+    alt_i: int
+    alt_d: int
+
+    pos_p: int
+    pos_i: int
+    pos_d: int
+
+    posr_p: int
+    posr_i: int
+    posr_d: int
+
+    navr_p: int
+    navr_i: int
+    navr_d: int
+
+    level_p: int
+    level_i: int
+    level_d: int
+
+    mag_p: int
+    mag_i: int
+    mag_d: int
+
+    vel_p: int
+    vel_i: int
+    vel_d: int
+
+class MspPidNames(object):
+    """Represents data values for the MSP_PIDNAMES command."""
+    names: tuple[str]
+
+class MspRawGps(object):
+    """Represents data values for the MSP_RAW_GPS command."""
+    fix: int
+
+    satellites: int
+
+    latitude: int
+
+    longitude: int
+
+    altitude: int
+
+    speed: int
+
+    ground_course: int
+
+def MspSetRawGps(NamedTuple):
+    """Represents data values for the MSP_SET_RAW_GPS command."""
+    fix: int
+
+    satellites: int
+
+    latitude: int
+
+    longitude: int
+
+    altitude: int
+
+    speed: int
+
+    ground_course: int
+
+class MspRawImu(object):
+    """Represents data values for the MSP_RAW_IMU command."""
+    acc: _Point3D[float]
+
+    gyro: _Point3D[float]
+
+    mag: _Point3D[float]
+
+class MspRc(object):
+    """Represents data values for the MSP_RC command."""
+    roll: int
+
+    pitch: int
+
+    yaw: int
+
+    throttle: int
+
+    aux: tuple[int]
+
+class MspSetRawRc(NamedTuple):
+    """Represents data values for the MSP_SET_RAW_RC command."""
+    roll: int
+
+    pitch: int
+
+    yaw: int
+
+    throttle: int
+
+    aux1: int
+
+    aux2: int
+
+    aux3: int
+
+    aux4: int
+
+class MspRcTuning(object):
+    """Represents data values for the MSP_RC_TUNING command."""
+    rate: int
+
+    expo: int
+
+    roll_pitch_rate: int
+
+    yaw_rate: int
+
+    dynamic_throttle_pid: int
+
+    throttle_mid: int
+
+    throttle_expo: int
+
+class MspServo(object):
+    """Represents data values for the MSP_SERVO command."""
+    values: tuple[int]
+
+class MspServoConf(object):
+    """Represents data values for the MSP_SERVO_CONF command."""
+    values: tuple[int]
+
+class MspSetServoConfItem(NamedTuple):
+    """Represents data values for the MSP_SET_SERVO_CONF command."""
+    min: int
+
+    max: int
+
+    middle: int
+
+    rate: int
+
+class MspStatus(object):
+    """Represents data values for the MSP_STATUS command."""
+    cycle_time: int
+
+    i2c_errors: int
+
+    sensors: tuple[int]
+
+    flag: int
+
+    global_conf: int
+
+class MspWaypoint(object):
+    """Represents data values for the MSP_WP command."""
+    number: int
+
+    latitude: int
+
+    longitude: int
+
+    alt_hold: int
+
+    heading: int
+
+    time_to_stay: int
+
+    flag: int
