3ac5ba784de23017076f247ca20433b6c319d2dc
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-13 00:51:44 +0200

    feat: move all config classes to single module file

--- wiiproxy/config/__init__.py
+++
@@ -1,6 +0,0 @@
-from .command_priority import CommandPriority
-
-from .multiwii_box        import MultiWiiBox, MultiWiiBoxState
-from .multiwii_capability import MultiWiiCapability
-from .multiwii_multitype  import MultiWiiMultitype
-from .multiwii_sensor     import MultiWiiSensor
