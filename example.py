9280718da8e5e8a3c1e12a25fbbed04c88471d37
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-13 22:02:14 +0200

    feat: update example script

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
 from wiiproxy.msp_message import MspMessage
-from wiiproxy.msp_data    import MspIdent, MspRawImu
 
 print(f'Header preamble: {MspMessage.HEADER_PREAMBLE}')
 print(f'Incoming header: {MspMessage.INCOMING_HEADER}')
