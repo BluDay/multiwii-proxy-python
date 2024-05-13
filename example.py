c23dcb09ce7d4a3b6b01132fdd3a6b5b7010c82d
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-13 17:19:50 +0200

    feat: remove unnecessary path updating code from example script

--- example.py
+++ example.py
@@ -1,10 +1,5 @@
 #! /usr/bin/env python3
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from wiiproxy.msp_message import MspMessage
 from wiiproxy.msp_data    import MspIdent, MspRawImu
 
