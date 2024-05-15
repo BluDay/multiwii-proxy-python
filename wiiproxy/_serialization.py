47101ce5a4895872c81e136bbc708762792d8f9e
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-15 22:33:27 +0200

    feat: change return-type type-hint for write serialization methods

--- wiiproxy/_serialization.py
+++ wiiproxy/_serialization.py
@@ -1,3 +1,5 @@
+from typing import NoReturn
+
 def read_int8(buffer: bytes, offset: int = 0) -> int:
     """Deserializes the provided bytes to a signed 8-bit integer.
 
@@ -57,7 +59,7 @@ def read_uint32(buffer: bytes, offset: int = 0) -> int:
     """
     return read_int32(buffer, offset) & 0xffffffff
 
-def write_int8(buffer: bytes, value: int, offset: int = 0) -> bytes:
+def write_int8(buffer: bytes, value: int, offset: int = 0) -> NoReturn:
     """Writes the provided value to the byte buffer as a signed 8-bit integer.
 
     This function serializes the provided signed 8-bit integer value and writes
@@ -65,7 +67,7 @@ def write_int8(buffer: bytes, value: int, offset: int = 0) -> bytes:
     """
     buffer[offset] = value
 
-def write_int16(buffer: bytes, value: int, offset: int = 0) -> bytes:
+def write_int16(buffer: bytes, value: int, offset: int = 0) -> NoReturn:
     """Writes the provided value to the byte buffer as a signed 16-bit integer.
 
     This function serializes the provided signed 16-bit integer value and writes
@@ -73,7 +75,7 @@ def write_int16(buffer: bytes, value: int, offset: int = 0) -> bytes:
     """
     pass
 
-def write_int32(buffer: bytes, value: int, offset: int = 0) -> bytes:
+def write_int32(buffer: bytes, value: int, offset: int = 0) -> NoReturn:
     """Writes the provided value to the byte buffer as a signed 32-bit integer.
 
     This function serializes the provided signed 32-bit integer value and writes
@@ -81,7 +83,7 @@ def write_int32(buffer: bytes, value: int, offset: int = 0) -> bytes:
     """
     pass
 
-def write_uint8(buffer: bytes, value: int, offset: int = 0) -> bytes:
+def write_uint8(buffer: bytes, value: int, offset: int = 0) -> NoReturn:
     """Writes the provided value to the byte buffer as a unsigned 8-bit integer.
 
     This function serializes the provided unsigned 8-bit integer value and writes
@@ -89,7 +91,7 @@ def write_uint8(buffer: bytes, value: int, offset: int = 0) -> bytes:
     """
     write_int8(value & 0xff)
 
-def write_uint16(buffer: bytes, value: int, offset: int = 0) -> bytes:
+def write_uint16(buffer: bytes, value: int, offset: int = 0) -> NoReturn:
     """Writes the provided value to the byte buffer as a unsigned 16-bit integer.
 
     This function serializes the provided unsigned 16-bit integer value and writes
@@ -97,7 +99,7 @@ def write_uint16(buffer: bytes, value: int, offset: int = 0) -> bytes:
     """
     write_int16(value & 0xffff)
 
-def write_uint32(buffer: bytes, value: int, offset: int = 0) -> bytes:
+def write_uint32(buffer: bytes, value: int, offset: int = 0) -> NoReturn:
     """Writes the provided value to the byte buffer as a unsigned 32-bit integer.
 
     This function serializes the provided unsigned 32-bit integer value and writes
