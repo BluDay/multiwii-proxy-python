0ea82b9575dd88aedd560970296036eb7462d5b4
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-16 09:15:22 +0200

    feat: update example script

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-from wiiproxy.msp_commands import MSP_IDENT
+from wiiproxy.msp_commands import MSP_IDENT, MSP_MISC, MSP_RC
 from wiiproxy.msp_message  import MspMessage
 
 print(f'Header preamble: {MspMessage.HEADER_PREAMBLE}')
@@ -8,3 +8,5 @@ print(f'Incoming header: {MspMessage.INCOMING_HEADER}')
 print(f'Outgoing header: {MspMessage.OUTGOING_HEADER}')
 
 print(MSP_IDENT)
+print(MSP_MISC)
+print(MSP_RC)
