5bcf2d70234a4985287c293646a94039585c36eb
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-17 15:01:08 +0200

    feat: improve documentation for the MspBoxItem class

--- wiiproxy/data/box.py
+++ wiiproxy/data/box.py
@@ -15,14 +15,42 @@ class MspBoxIds:
 
 @dataclass
 class MspBoxItem(NamedTuple):
-    """Represents data values for the MSP_SET_BOX command."""
+    """Represents data values for the MSP_SET_BOX command.
+
+    This class encapsulates the configuration of auxiliary (aux) control boxes in a
+    MultiWii flight controller. Each box can be assigned a specific function or mode,
+    and its state can be set to the following values:
+
+        * (Unselected) (Empty)
+        * LOW (Low)
+        * MID (Mid)
+        * HIGH (High)
+
+    Attributes
+    ----------
+    aux 1: MultiWiiStateBox
+        The state value for the first auxiliary function.
+    aux 2: MultiWiiStateBox
+        The state value for the second auxiliary function.
+    aux 3: MultiWiiStateBox
+        The state value for the third auxiliary function.
+    aux 4: MultiWiiStateBox
+        The state value for the fourth auxiliary function.   
+    """
     aux1: MultiWiiBoxState
     aux2: MultiWiiBoxState
     aux3: MultiWiiBoxState
     aux4: MultiWiiBoxState
      
     def compile(self) -> int:
-        """Compiles all of the set box state values to a single unsigned integer value."""
+        """Compiles all of the box state values into a single unsigned integer value.
+
+        Returns
+        -------
+        int
+            The compiled integer value representing the combined state of all auxiliary
+            control boxes.
+        """
         pass
 
 @dataclass
