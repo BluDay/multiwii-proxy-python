c1f919ff90137745817aac49b8f79648b4e0cc4e
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-17 13:05:05 +0200

    feat: make custom base types tuples

--- wiiproxy/data/_types.py
+++ wiiproxy/data/_types.py
@@ -1,30 +1,30 @@
 from dataclasses import dataclass
-from typing      import Generic, TypeVar
+from typing      import Generic, NamedTuple, TypeVar
 
 T = TypeVar('T')
 
 @dataclass
-class Coord2D(Generic[T]):
+class Coord2D(NamedTuple, Generic[T]):
     """Represents 2D coordinates with longitude and latitude values."""
     latitude: T
 
     longitude: T
 
 @dataclass
-class PidValues(Generic[T]):
+class PidValues(NamedTuple, Generic[T]):
     """Represents PID values."""
     p: T
     i: T
     d: T
 
 @dataclass
-class Point2D(Generic[T]):
+class Point2D(NamedTuple, Generic[T]):
     """Represents a 2D point."""
     x: T
     y: T
 
 @dataclass
-class Point3D(Generic[T]):
+class Point3D(NamedTuple, Generic[T]):
     """Represents a 3D point."""
     x: T
     y: T
