c791f4ee56436f0da75c4afb1260f17b0ffea00c
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-17 13:05:57 +0200

    feat: remove dataclass decorators and import statement

--- wiiproxy/data/_types.py
+++ wiiproxy/data/_types.py
@@ -1,29 +1,24 @@
-from dataclasses import dataclass
-from typing      import Generic, NamedTuple, TypeVar
+from typing import Generic, NamedTuple, TypeVar
 
 T = TypeVar('T')
 
-@dataclass
 class Coord2D(NamedTuple, Generic[T]):
     """Represents 2D coordinates with longitude and latitude values."""
     latitude: T
 
     longitude: T
 
-@dataclass
 class PidValues(NamedTuple, Generic[T]):
     """Represents PID values."""
     p: T
     i: T
     d: T
 
-@dataclass
 class Point2D(NamedTuple, Generic[T]):
     """Represents a 2D point."""
     x: T
     y: T
 
-@dataclass
 class Point3D(NamedTuple, Generic[T]):
     """Represents a 3D point."""
     x: T
