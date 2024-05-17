e0ed94ec9ca9952a6721aac955990640c32be1e3
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-17 13:34:36 +0200

    docs: document the class attributes as well for struct-like type classes

--- wiiproxy/data/_types.py
+++ wiiproxy/data/_types.py
@@ -3,13 +3,31 @@ from typing import Generic, NamedTuple, TypeVar
 T = TypeVar('T')
 
 class Coord2D(NamedTuple):
-    """Represents 2D geographics coordinates with longitude and latitude values."""
+    """Represents 2D geographics coordinates with longitude and latitude values.
+
+    Attributes
+    ----------
+    latitude : float
+        The latitude value of the coordinate.
+    longitude : float
+        The longitude value of the coordinate.
+    """
     latitude: float
 
     longitude: float
 
 class PidValues(NamedTuple, Generic[T]):
-    """Represents PID values."""
+    """Represents PID values.
+    
+    Attributes
+    ----------
+    proportional : T
+        The proportional (P) component of the PID controller.
+    integral : T
+        The integral (I) component of the PID controller.
+    derivative : T 
+        The derivative (D) component of the PID controller.
+    """
     proportional: T
 
     integral: T
@@ -17,12 +35,30 @@ class PidValues(NamedTuple, Generic[T]):
     derivative: T
 
 class Point2D(NamedTuple, Generic[T]):
-    """Represents a 2D point."""
+    """Represents a 2D point.
+
+    Attributes
+    ----------
+    x : T
+        The x-coordinate of the point.
+    y : T
+        The y-coordinate of the point.
+    """
     x: T
     y: T
 
 class Point3D(NamedTuple, Generic[T]):
-    """Represents a 3D point."""
+    """Represents a 3D point.
+    
+    Attributes
+    ----------
+    x : T
+        The x-coordinate of the point.
+    y : T
+        The y-coordinate of the point.
+    z : T
+        The z-coordinate of the point.
+    """
     x: T
     y: T
     z: T
