1e2de68360d3986ff03f0fdd3d838da3da282e3d
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-20 12:48:31 +0200

    docs: finish documentation of functions in the messaging submodule

--- wiiproxy/messaging.py
+++ wiiproxy/messaging.py
@@ -13,17 +13,38 @@ OUTGOING_HEADER: Final[bytes] = b'$M>' # 0x24, 0x4d, 0x3e
 def decode_names(data: tuple) -> tuple[str]:
     """Decodes the deserialized string value and splits it to a tuple.
 
+    Parameters
+    ----------
+    data : tuple
+        The `struct` unpacked payload tuple to decode.
+
     Raises
     ------
     TypeError
         If `data` is not of type `tuple`.
     UnicodeDecodeError
         If the bytes cannot be decoded into ASCII characters.
+
+    Returns
+    -------
+    tuple[str]
+        A tuple of decoded names.
     """
     return tuple(data[0].decode('ascii').split(';'))
 
 def calculate_checksum(payload: bytes) -> int:
-    """Calculates the checksum for the payload using an XOR CRC."""
+    """Calculates the checksum for the payload using an XOR CRC.
+
+    Parameters
+    ----------
+    payload : bytes
+        Bytes of the full payload (including the command code and size).
+
+    Returns
+    -------
+    int
+        The checksum for the provided payload.
+    """
     checksum = 0
 
     for byte in data: checksum ^= byte
