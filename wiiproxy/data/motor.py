28ad20ff69042a4b4fabbdab4aa0c80262946f32
BluDay <19403893+BluDay@users.noreply.github.com>
2024-05-21 11:55:14 +0200

    docs: complete doucmentation for the data.motor submodule

--- wiiproxy/data/motor.py
+++ wiiproxy/data/motor.py
@@ -3,7 +3,31 @@ from typing      import Self
 
 @dataclass
 class MspMotor:
-    """Represents data values for the MSP_MOTOR command."""
+    """Represents data values for the MSP_MOTOR command.
+
+    This class encapsulates the motor speed values for up to eight motors in a
+    MultiWii flight controller. Each motor's speed is represented as an integer
+    value.
+
+    Attributes
+    ----------
+    motor 1: int
+        The speed value for motor 1.
+    motor 2: int
+        The speed value for motor 2.
+    motor 3: int
+        The speed value for motor 3.
+    motor 4: int
+        The speed value for motor 4.
+    motor 5: int
+        The speed value for motor 5.
+    motor 6: int
+        The speed value for motor 6.
+    motor 7: int
+        The speed value for motor 7.
+    motor 8: int
+        The speed value for motor 8.
+    """
     motor1: int
     motor2: int
     motor3: int
@@ -15,12 +39,46 @@ class MspMotor:
 
     @classmethod
     def parse(cls, data: tuple) -> Self:
+        """Parses a tuple of data values obtained from `struct.unpack` and returns an
+        instance of the `MspMotor` class.
+
+        Parameters
+        ----------
+        data : tuple
+            A tuple containing unpacked data values.
+
+        Returns
+        -------
+        MspMotor
+            An instance of the `MspMotor` class populated with the parsed data.
+        """
         return cls(*data)
 
 @dataclass
 class MspMotorPins(MspMotor):
-    """Represents data values for the MSP_MOTOR_PINS command."""
+    """Represents data values for the MSP_MOTOR_PINS command.
+
+    This class extends `MspMotor` to provide the motor pin values for up to eight motors
+    in a MultiWii flight controller. It inherits all attributes and methods from `MspMotor`.
+
+    Attributes
+    ----------
+    Inherits all attributes from `MspMotor`.
+    """
 
     @classmethod
     def parse(cls, data: tuple) -> Self:
+        """Parses a tuple of data values obtained from `struct.unpack` and returns an
+        instance of the `MspMotorPins` class.
+
+        Parameters
+        ----------
+        data : tuple
+            A tuple containing unpacked data values.
+
+        Returns
+        -------
+        MspMotorPins
+            An instance of the `MspMotorPins` class populated with the parsed data.
+        """
         return cls(*data)
