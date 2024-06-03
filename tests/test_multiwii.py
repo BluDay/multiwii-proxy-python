1fb8d158cff6b514d7020fb84740de5f6d641c99
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-03 11:16:37 +0200

    test: update TestMultiWii class

--- tests/test_multiwii.py
+++ tests/test_multiwii.py
@@ -4,9 +4,6 @@ from pytest import raises
 from typing import NoReturn
 
 class TestMultiWii(object):
-    """Unit test class for the MultiWii class."""
-    
-    def test_create_instance_that_raises_exception(self) -> NoReturn:
-        """Creates a MultiWii instance that raises the only exception."""
+    def test_instantiation_that_raises_a_type_error(self) -> NoReturn:
         with raises(TypeError):
             fc = MultiWii(serial=None)
