fdf5b37a05614c1a551b0c3427876d06258694ce
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-03 11:34:30 +0200

    style: simplify setup.py and update module description

--- setup.py
+++ setup.py
@@ -2,25 +2,16 @@
 
 from setuptools import setup
 
-from wiiproxy import (
-    __author__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__
-)
-
-name = __title__.lower()
-
 setup(
-    name=name,
-    version=__version__,
-    description=__description__,
-    url=__url__,
-    author=__author__,
-    license=__license__,
-    packages=[name],
+    name='WiiProxy',
+    version='1.0',
+    description='A simple and user-friendly Python 3 module for MultiWii-based drones.',
+    url='https://github.com/BluDay/wiiproxy',
+    author='BluDay',
+    license='MIT',
+    packages=(
+        'wiiproxy'
+    ),
     requires=(
         'pyserial==3.4,3.5',
         'pytest>=8.1.1'
