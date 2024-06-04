bd422ffae8578ec6fc002a511e71eb5305c5dbbc
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-04 22:56:55 +0200

    feat: remove tests directory

--- tests/test_multiwii.py
+++
@@ -1,9 +0,0 @@
-from wiiproxy.multiwii import MultiWii
-
-from pytest import raises
-from typing import NoReturn
-
-class TestMultiWii(object):
-    def test_instantiation_that_raises_a_type_error(self) -> NoReturn:
-        with raises(TypeError):
-            fc = MultiWii(serial=None)
