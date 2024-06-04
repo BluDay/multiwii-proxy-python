bda976464ef9a3dd8c1bb2a3eb6dacb5f6e1a2a9
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-04 22:56:31 +0200

    feat: update info in package and setup file

--- setup.py
+++ setup.py
@@ -4,16 +4,11 @@ from setuptools import setup
 
 setup(
     name='WiiProxy',
-    version='1.0',
+    version='3.0',
     description='A simple and user-friendly Python 3 module for MultiWii-based drones.',
-    url='https://github.com/BluDay/wiiproxy',
+    url='https://bluday.github.io/wiiproxy',
     author='BluDay',
     license='MIT',
-    packages=(
-        'wiiproxy'
-    ),
-    requires=(
-        'pyserial==3.4,3.5',
-        'pytest>=8.1.1'
-    )
+    packages=('wiiproxy'),
+    requires=('pyserial==3.4,3.5')
 )
