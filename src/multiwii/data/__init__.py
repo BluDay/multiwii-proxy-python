a650dca27484d044b3abdfeb3b88910be1d78913
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-06 14:12:06 +0200

    style: use one-line import statements in data subpackage

--- src/multiwii/data/__init__.py
+++ src/multiwii/data/__init__.py
@@ -1,54 +1,17 @@
-from ._types import (
-    Coordinates,
-    Pid,
-    Point2D,
-    Point3D
-)
+from ._types import Coordinates, Pid, Point2D, Point3D
 
-from .box import (
-    MspBox,
-    MspBoxIds,
-    MspBoxItem,
-    MspBoxNames
-)
+from .box import MspBox, MspBoxIds, MspBoxItem, MspBoxNames
 
-from .info import (
-    MspAnalog,
-    MspIdent,
-    MspMisc,
-    MspSetMisc,
-    MspStatus
-)
+from .info import MspAnalog, MspIdent, MspMisc, MspSetMisc, MspStatus
 
-from .motor import (
-    MspMotor,
-    MspMotorPins
-)
+from .motor import MspMotor, MspMotorPins
 
-from .navigation import (
-    MspCompGps,
-    MspRawGps,
-    MspWaypoint
-)
+from .navigation import MspCompGps, MspRawGps, MspWaypoint
 
-from .pid import (
-    MspPid,
-    MspPidNames
-)
+from .pid import MspPid, MspPidNames
 
-from .rc import (
-    MspRc,
-    MspRcTuning
-)
+from .rc import MspRc, MspRcTuning
 
-from .servo import (
-    MspServo,
-    MspServoConf,
-    MspServoConfItem
-)
+from .servo import MspServo, MspServoConf, MspServoConfItem
 
-from .telemetry import (
-    MspAltitude,
-    MspAttitude,
-    MspRawImu
-)
+from .telemetry import MspAltitude, MspAttitude, MspRawImu
